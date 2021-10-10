beb22f2df36d95937fe531c88bad405b8008400c
daniil_turov <turow.dany@yandex.ru>
2021-10-11 00:25:47 +0500

    Задача 4. Число наоборот 3

--- 15_IDE/04_reverse_num/main.py
+++ 15_IDE/04_reverse_num/main.py
@@ -1,3 +1,31 @@
-# TODO здесь писать код
+def сhanging(number):
+  start = True
+  whole =''
+  fraction = ''
+  for symbol in number:
+    if symbol == '.':
+      start = False
+    elif start:
+      whole += symbol
+    else:
+      fraction += symbol
+  reverse_number = str(revers(whole)) + '.' + str(revers(fraction))
+  return reverse_number
+
+def revers(number):
+  revers_number = ''
+  while int(number) > 0:
+      revers_number += str(int(number) % 10)
+      number = int(number) // 10
+  return(revers_number)
+
+def main_menu():
+  first_number = (input("Введите первое число: "))
+  second_number = (input("Введите второе число: "))
+  print('\nПервое число наоборот: ', сhanging(first_number))
+  print('Второе число наоборот: ', сhanging(second_number))
+  print('Сумма: ', float(сhanging(first_number)) + float(сhanging(second_number)))
+
+main_menu()
 
 
