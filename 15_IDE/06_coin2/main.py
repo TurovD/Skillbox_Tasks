47c2ffc9373de9ce1e59e023502596165706f4a5
daniil_turov <turow.dany@yandex.ru>
2021-10-11 13:53:18 +0500

    Задача 6. Монетка 2

--- 15_IDE/06_coin2/main.py
+++ 15_IDE/06_coin2/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+def coin(coins_x,coins_y,r):
+    if (abs(coins_x) <= r) and (abs(coins_y) <= r) :
+        print('\nМонетка где-то рядом')
+    else:
+        print('\nМонетки в области нет')
+
+coin(float(input('Введите координаты монетки \nX: ')),float(input('Y: ')),float(input('Введите радиус: ')))
