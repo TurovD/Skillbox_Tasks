1990867d25bebb0592c753b6dbd0dc0121f5928f
daniil_turov <turow.dany@yandex.ru>
2021-10-11 13:37:20 +0500

    Задача 5. Наименьший делитель.

--- 15_IDE/05_least_divisor/main.py
+++ 15_IDE/05_least_divisor/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+def min_divider(number, divider=2):
+    if number % divider == 0:
+        print('Наименьший делитель, отличный от единицы: ', divider)
+    else:
+        min_divider(number, divider + 1)
+
+number = int(input('Введите число: '))
+min_divider(number)
