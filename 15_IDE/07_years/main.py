dcd4d377ab9ed233bd139f3218b2cfe0d401a143
daniil_turov <turow.dany@yandex.ru>
2021-10-11 14:50:55 +0500

    Задача 7. Годы

--- 15_IDE/07_years/main.py
+++ 15_IDE/07_years/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+def years(start,finish):
+    answer = []
+    for number in range(start,finish+1):
+        string = str(number)
+        multitude = set(string)
+        for symbol in multitude:
+            if string.count(symbol) > 2:
+                answer.append(number)
+    print('Года от', start, 'до', finish,'с тремя одинаковыми цифрами: ')
+    print(*answer,sep='\n')
+
+years(int(input('Введите первый год: ')),int(input('Введите второй год: ')))
