3c865f5859a6c6dc9c4bab8a75884c8c4dc8b7c2
daniil_turov <turow.dany@yandex.ru>
2021-10-15 16:36:51 +0500

    Задача 2. Турнир

--- 16_Lists/02_tournament/main.py
+++ 16_Lists/02_tournament/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+#Человек с чётным индексом.
+# names_list = ['Артемий', 'Борис', 'Влад', 'Гоша', 'Дима', 'Евгений', 'Женя', 'Захар']
+# list_second_names = names_list[::2]
+# for i in range(len(list_second_names)):
+#     print(list_second_names[i],end=' ')
+
+#Каждый второй из списка.
+names_list = ['Артемий', 'Борис', 'Влад', 'Гоша', 'Дима', 'Евгений', 'Женя', 'Захар']
+for i in range(1,len(names_list), 2):
+     print(names_list[i], end=' ')
