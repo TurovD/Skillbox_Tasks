5353ae0beec2f8a91613c7e3cdd9bf21df18b7bb
daniil_turov <turow.dany@yandex.ru>
2021-10-16 01:32:13 +0500

    Задача 8. Бегущие цифры

--- 16_Lists/08_running_nums/main.py
+++ 16_Lists/08_running_nums/main.py
@@ -1,1 +1,6 @@
 # TODO здесь писать код
+shift = int(input('Сдвиг: '))
+first_list = [int(i) for i in input('Введите числа для изначального списка через пробел: ').split()]
+final_list = first_list[-shift:] + first_list[:-shift]
+print('\nИзначальный список: ',first_list)
+print('Сдвинутый список: ',final_list)
