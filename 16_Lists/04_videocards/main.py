3f10b17f304b0f1d8c8f6555111bb2447692a5f8
daniil_turov <turow.dany@yandex.ru>
2021-10-15 17:30:43 +0500

    Задача 4. Видеокарты

--- 16_Lists/04_videocards/main.py
+++ 16_Lists/04_videocards/main.py
@@ -1,3 +1,10 @@
-# TODO здесь писать код
 
 
+cards = int(input('Введите количество видеокарт: '))
+list_cards = []
+for i in range(1, cards+1):
+    print(i,'Видеокарта: ',end='')
+    list_cards.append(int(input()))
+print('Старый список видеокарт: ', list_cards, sep =' ')
+new_cards_list = [card for card in list_cards if card!=max(list_cards)]
+print('Новый список видеокарт: ', new_cards_list )
