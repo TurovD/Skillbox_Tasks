2f77e30585376c00c3dbc8b9668f851696d56264
daniil_turov <turow.dany@yandex.ru>
2021-10-15 16:36:51 +0500

    Задача 2. Турнир

--- 16_Lists/03_cells/main.py
+++ 16_Lists/03_cells/main.py
@@ -1,1 +1,11 @@
 # TODO здесь писать код
+cages = int(input('Введите количество клеток: '))
+list_cages = []
+list_effectiveness = []
+for i in range(1, cages+1):
+    print('Эффективность',i,'клетки: ',end='')
+    list_cages.append(input())
+for effectiveness in range(len(list_cages)):
+    if effectiveness > int(list_cages[effectiveness]):
+        list_effectiveness.append(list_cages[effectiveness])
+print('Неподходящие значения: ', *list_effectiveness, sep =' ')
