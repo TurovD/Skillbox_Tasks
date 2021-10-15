d2c2ab594c57e77ee66e8d106aa7ca869340d4c3
daniil_turov <turow.dany@yandex.ru>
2021-10-16 01:55:36 +0500

    Задача 9. Анализ слова 2

--- 16_Lists/09_word_analysis_2/main.py
+++ 16_Lists/09_word_analysis_2/main.py
@@ -1,1 +1,7 @@
 # TODO здесь писать код
+word = list(input('Введите слово: '))
+compare_word = word[::-1]
+if compare_word == word:
+    print('\nСлово является палиндромом')
+else:
+    print('\nСлово не является палиндромом')
--- 16_Lists/10_sort/main.py
+++ 16_Lists/10_sort/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def sort_list(first_list):
+    for list in range (len(first_list)-1):
+        for compare_number in range(len(first_list)-1-list):
+            if first_list[compare_number] > first_list[compare_number+1]:
+                first_list[compare_number],first_list[compare_number+1]=first_list[compare_number+1],first_list[compare_number]
+    return(first_list)
+
+
+first_list = [int(i) for i in input('Введите числа для изначального списка через пробел: ').split()]
+sort_list(first_list)
+print('\nОтсортированный список: ',first_list)
