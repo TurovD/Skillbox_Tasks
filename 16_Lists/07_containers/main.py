f241d2b07988bb52716884ce89c2e1a99bb759c7
daniil_turov <turow.dany@yandex.ru>
2021-10-16 01:12:59 +0500

    Задача 7. Контейнеры

--- 16_Lists/07_containers/main.py
+++ 16_Lists/07_containers/main.py
@@ -1,1 +1,11 @@
 # TODO здесь писать код
+containers = (int(input('Кол-во контейнеров: ')))
+weight_containers,index = [],1
+for _ in range(1,containers+1):
+    weight_cont = (int(input('Введите вес контейнера: ')))
+    weight_containers.append(weight_cont)
+new_containers = int(input('Введите вес нового контейнера: '))
+for i in range (len(weight_containers)):
+    if int(weight_containers[i])>=new_containers:
+        index+=1
+print('Номер, куда встанет новый контейнер: ',index)
