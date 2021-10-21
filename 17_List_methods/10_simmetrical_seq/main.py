2aff706a46fde7fbfa6504c1f5ac1f503d380ff3
daniil_turov <turow.dany@yandex.ru>
2021-10-21 16:40:08 +0500

    Задача 10. Симметричная последовательность

--- 17_List_methods/10_simmetrical_seq/main.py
+++ 17_List_methods/10_simmetrical_seq/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+
+number_num = int(input('Введите кол-во чисел: '))
+number_list = []
+for i in range(number_num):
+    number_list.append(int(input('Число: ')))
+print('\nПоследовательность: ',*number_list)
+count_number = 0
+while number_list != number_list[::-1]:
+    number_list.insert(number_num, number_list[count_number])
+    count_number += 1
+print('Нужно дописать чисел: ',count_number)
+print('Сами числа: ',*number_list[:count_number][::-1])
