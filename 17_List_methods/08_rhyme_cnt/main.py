ff1d5f875547f4a3028ea5f15a55911b47dc111c
daniil_turov <turow.dany@yandex.ru>
2021-10-21 15:13:58 +0500

    Задача 8. Считалка

--- 17_List_methods/08_rhyme_cnt/main.py
+++ 17_List_methods/08_rhyme_cnt/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+
+def game():
+    out = 0
+    for _ in range(len(people_list) - 1):
+        print('\nТекущий круг людей', people_list )
+        start_count = out % len(people_list )
+        out = (start_count + number - 1) % len(people_list )
+        print('Начало счёта с номера', people_list [start_count])
+        print('Выбывает человек под номером', people_list [out])
+        people_list.remove(people_list[out])
+    print('\nОстался человек под номером: ', people_list[0] )
+
+
+people_count = int(input('Кол-во человек: '))
+number = int(input('Какое число в считалке? '))
+print('Значит, выбывает каждый', number, 'человек')
+people_list = list(range(1,people_count+1))
+game()
