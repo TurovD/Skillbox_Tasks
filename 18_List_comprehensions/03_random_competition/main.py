3bab5026220a358cd4aa2891acbda80c120fe664
daniil_turov <turow.dany@yandex.ru>
2021-10-29 14:59:12 +0500

    Задача 3. Случайные соревнования

--- 18_List_comprehensions/03_random_competition/main.py
+++ 18_List_comprehensions/03_random_competition/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+
+import random
+
+first_team = [round(random.uniform(5, 10), 2) for num in range(20)]
+second_team = [round(random.uniform(5, 10), 2) for num in range(20)]
+winers = [max(num) for num in zip(first_team,second_team)]
+print ('Первая команда: ', first_team)
+print ('Вторая команда: ', second_team)
+print('Победители тура: ',winers)
