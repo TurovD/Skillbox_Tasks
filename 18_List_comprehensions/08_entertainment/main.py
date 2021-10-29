68fa53fbc80140edbb170406ed59813be976c69e
daniil_turov <turow.dany@yandex.ru>
2021-10-29 22:32:28 +0500

    Задача 8. Развлечение

--- 18_List_comprehensions/08_entertainment/main.py
+++ 18_List_comprehensions/08_entertainment/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+import random
+
+
+N_number = int(input('Кол-во палок: '))
+sticks = ['|'] * N_number
+for i in range(1, int(input('Кол-во бросков: '))+1):
+    print('Бросок ', i, '.', sep='', end=' ')
+    for downed_stick in range(int(input('Сбиты палки с номера '))-1, int(input('по номер '))):
+        sticks[downed_stick] = '.'
+
+print('Результат: ',''.join(sticks))
