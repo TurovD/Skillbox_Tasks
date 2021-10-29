9914440985734ccdfccdc19aa502d102995e0678
daniil_turov <turow.dany@yandex.ru>
2021-10-29 22:59:37 +0500

    Задача 9. Список списков

--- 18_List_comprehensions/09_list_of_lists_2/main.py
+++ 18_List_comprehensions/09_list_of_lists_2/main.py
@@ -1,4 +1,6 @@
 nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+clean_list = [number for nested_list in nice_list for nested_number in nested_list for number in nested_number]
+
+print(clean_list)
