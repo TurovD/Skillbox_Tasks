f2db397126ee9137caffdfb56a27b2a01d7ab719
daniil_turov <turow.dany@yandex.ru>
2021-10-30 01:53:17 +0500

    Задача 10. Шифр Цезаря (сделайте по желанию)

--- 18_List_comprehensions/10_caesar_cipher/main.py
+++ 18_List_comprehensions/10_caesar_cipher/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+
+def caesar(text, shift):
+    alphabet = ['а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ',
+                'ы','ь','э','ю','я']
+    shifted_text = ([' ' if i == ' ' else (alphabet[(alphabet.index(i)+shift)-33] if (alphabet.index(i)+shift)>32
+                                            else alphabet[(alphabet.index(i)+shift)])  for i in text])
+    print('Зашифрованное сообщение: ',''.join(shifted_text))
+
+text = [letter for letter in input('Введите сообщение: ').lower()]
+shift = int(input('Введите сдвиг: '))
+caesar(text,shift)
+
