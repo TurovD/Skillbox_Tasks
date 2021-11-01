4e03f9fd9404305cdbaa593cde55963bf0a41080
daniil_turov <turow.dany@yandex.ru>
2021-11-01 23:40:59 +0500

    Задача 2. Самое длинное слово

--- 19_Strings/02_longest_word/main.py
+++ 19_Strings/02_longest_word/main.py
@@ -1,1 +1,5 @@
-# TODO здесь писать код
+text = input('Введите строку: ').split()
+biggest_word = (max(text, key=len))
+len_word = len(biggest_word)
+print('Самое длинное слово:', biggest_word)
+print('Длина слова:', len_word, 'букв')
