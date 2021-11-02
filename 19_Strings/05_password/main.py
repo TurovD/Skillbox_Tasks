eff4939a976b2e291223276e6eb8012ad866b136
daniil_turov <turow.dany@yandex.ru>
2021-11-02 22:37:50 +0500

    Задача 5. Пароль

--- 19_Strings/05_password/main.py
+++ 19_Strings/05_password/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+def input_password():
+    password = list(input('\nПридумайте пароль: '))
+    check_password(password)
+
+def check_password(password):
+    length_password =  len(password)
+    upper_letter = sum(up_letter.isupper() for up_letter in password)
+    count_numbers =  sum(number.isdigit() for number in password)
+    if (length_password >= 8) and (upper_letter >= 1) and (count_numbers >= 3):
+        print('Это надёжный пароль!')
+    else:
+        print ('Пароль ненадёжный. Попробуйте ещё раз.')
+        input_password()
+
+
+
+input_password()
+
+
