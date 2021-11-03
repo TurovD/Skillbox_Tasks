ff871e369f5b290c1e21793d2bc6bafcf985babf
daniil_turov <turow.dany@yandex.ru>
2021-11-03 20:13:51 +0500

    Задача 6. Сжатие

--- 19_Strings/06_compression/main.py
+++ 19_Strings/06_compression/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+text = input('Введите строку: ')
+code_text = ''
+for letter in text:
+    if not len(code_text):
+        code_text = letter + '1'
+    else:
+        if letter == code_text[-2]:
+            code_text = '{}{}'.format(code_text[:-1], int(code_text[-1]) + 1)
+        else:
+            code_text = '{}{}{}'.format(code_text, letter, '1')
+print(code_text)
