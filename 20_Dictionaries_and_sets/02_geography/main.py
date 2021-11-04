cd22b895f53eedf25d452f47b4f9f5689e0b4aa6
daniil_turov <turow.dany@yandex.ru>
2021-11-04 19:15:10 +0500

    Задача 2. География

--- 20_Dictionaries_and_sets/02_geography/main.py
+++ 20_Dictionaries_and_sets/02_geography/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+set_countries = {}
+for count_country in range(0, int(input('Кол-во стран: '))):
+    value = input('{} страна: '.format(count_country+1)).split()
+    for city_set in value[1:]:
+        set_countries[city_set] = value[0]
+for count in range(1, 4):
+    city = input('\n{} город: '.format(count))
+    country = set_countries.get(city)
+    if country:
+        print('Город {} расположен в стране {}.'.format(city,country))
+    else:
+        print('По городу {} данных нет.'.format(city))
