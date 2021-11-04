08998be03086edb9fe91c74015aecf1c4a0d2d37
daniil_turov <turow.dany@yandex.ru>
2021-11-04 10:44:02 +0500

    Задача 9. Сообщение

--- 19_Strings/09_message/main.py
+++ 19_Strings/09_message/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+message = input('Сообщение: ')
+special_symbol = True
+new_word = ''
+result = ''
+for elements in message:
+    if elements.isalpha():
+        special_symbol = False
+        new_word += elements
+    elif not elements.isalpha() and special_symbol:
+        result += elements
+    elif not elements.isalpha() and not special_symbol:
+        new_word = new_word[::-1]
+        result += new_word
+        result += elements
+        new_word = ''
+if new_word != '':
+    new_word = new_word[::-1]
+    result += new_word
+print('Новое сообщение: ', result)
