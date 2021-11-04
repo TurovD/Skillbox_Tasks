6cca2192ed24d3605f309459a591e049bfbdb47d
daniil_turov <turow.dany@yandex.ru>
2021-11-04 23:21:38 +0500

    Задача 5. Гистограмма частоты 2

--- 20_Dictionaries_and_sets/05_frequency_hist_2/main.py
+++ 20_Dictionaries_and_sets/05_frequency_hist_2/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+import collections
+
+def histogram(string):
+    sym_dict = dict()
+    for sym in string:
+        if sym in sym_dict:
+            sym_dict[sym] += 1
+        else:
+            sym_dict[sym] = 1
+    return sym_dict
+
+text = input('Введите текст: ')
+hist = histogram(text)
+print('Оригинальный словарь частот:')
+for i_sym in sorted(hist.keys()):
+    print(i_sym, ':', hist[i_sym])
+
+
+inverted_dictionary = dict()
+for letter, number in collections.Counter(text).items():
+    inverted_dictionary.setdefault(number, []).append(letter)
+print('\nИнвертированный словарь частот: ')
+for inverted in inverted_dictionary:
+    print(inverted, ':', inverted_dictionary[inverted])
