61169af4b342766d2ec068285774435bf71fab4e
daniil_turov <turow.dany@yandex.ru>
2021-11-05 00:08:52 +0500

    Задача 6. Словарь синонимов

--- 20_Dictionaries_and_sets/06_synonym_dict/main.py
+++ 20_Dictionaries_and_sets/06_synonym_dict/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+pair_dict = {}
+
+for i in range(int(input('Введите количество пар слов: '))):
+    first_word, second_word = input('{} пара: '.format(i + 1)).title().split(' - ')
+    pair_dict[first_word] = second_word
+    pair_dict[second_word] = first_word
+print()
+while True:
+    word = input('Введите слово: ').title()
+    if word in pair_dict:
+        print('Синоним: ', pair_dict[word])
+        break
+    else:
+        print('Такого слова в словаре нет.')
+
+
+
+
