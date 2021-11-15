3b6c7eedea085da3b4b3dbfd19bfdcc372c6b62b
daniil_turov <turow.dany@yandex.ru>
2021-11-15 22:53:39 +0500

    22.6 Домашняя работа

--- 22_Functions/03_fibonacci/main.py
+++ 22_Functions/03_fibonacci/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+def fibonacci(n):
+    if n in (1, 2):
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+print('Число:', fibonacci(int(input('Введите позицию числа в ряде Фибоначчи: '))))
