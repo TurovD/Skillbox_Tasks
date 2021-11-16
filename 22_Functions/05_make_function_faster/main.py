41409c0913ae63c6963cdcfb1a90e9477e5fb112
daniil_turov <turow.dany@yandex.ru>
2021-11-16 19:55:56 +0500

    22.6 Домашняя работа

--- 22_Functions/05_make_function_faster/main.py
+++ 22_Functions/05_make_function_faster/main.py
@@ -1,9 +1,16 @@
 def calculating_math_func(data):
-    result = 1
-    for index in range(1, data + 1):
-        result *= index
+    if data in list_of_factorials:
+        result = list_of_factorials[data]
+    else:
+        result = 1
+        for index in range(1, data + 1):
+            result *= index
+        list_of_factorials[data] = result
     result /= data ** 3
     result = result ** 10
     return result
 
-# TODO оптимизировать функцию
+
+list_of_factorials = {}
+for i in range (1, int(input('Сколько чисел вы хотите ввести? '))+1):
+    print('Получившиеся число: ', calculating_math_func(int(input('\nВведите {} число: '.format(i)))))
