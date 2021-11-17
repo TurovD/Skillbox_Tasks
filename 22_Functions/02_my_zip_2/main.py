cb7e008e9ef5222fde51c8c37a5e6269fa2f81ef
daniil_turov <turow.dany@yandex.ru>
2021-11-18 00:20:48 +0500

    22.6 Домашняя работа

--- 22_Functions/02_my_zip_2/main.py
+++ 22_Functions/02_my_zip_2/main.py
@@ -1,11 +1,1 @@
-def my_zip(*arg):
-    arg = list(map(list, arg))
-    try:
-        yield tuple(map(lambda x : x.pop(0), arg))
-        yield from my_zip(*arg)
-    except:
-        pass
-
-
-for a in my_zip({'a':1,'b':2,'c':3},[1,2,3,4,5],[3,4,5],"asdf"):
-    print(a)
+# TODO здесь писать код
